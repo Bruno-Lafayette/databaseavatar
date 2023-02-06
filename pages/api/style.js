cfb17368345288acd9a68c2acebc8ba9155adad2
Bruno.Lafayette <brunolafayette@rocketmail.com>
2023-02-06 17:19:25 -0300

    adicionado styles

---
+++ pages/api/style.js
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function Style(req, res){
+
+  res.json({
+      styles:  ["adventurer", "adventurerNeutral", "avataaars", "avataaarsNeutral", "bigEars", "bigEarsNeutral", "bigSmile", "bottts", "botttsNeutral", "croodles", "croodlesNeutral", "funEmoji", "thumbs", "icons", "identicon", "initials", "lorelei", "loreleiNeutral", "micah", "miniavs", "openPeeps", "personas", "pixelArt", "pixelArtNeutral", "shapes"]
+  });
+
+}
