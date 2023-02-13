22606689d139946a734132deb73a18db0b7a348c
Bruno.Lafayette <brunolafayette@rocketmail.com>
2023-02-13 15:12:11 -0300

    alterado adventurer

--- pages/api/character/adventurer.js
+++ pages/api/character/adventurer.js
@@ -4,7 +4,6 @@ export default function adventurer(req, res){
         eyebrows: ['variant10' , 'variant09' , 'variant08' , 'variant07' , 'variant06' , 'variant05' , 'variant04' , 'variant03' , 'variant02' , 'variant01' , 'variant15' , 'variant14' , 'variant13' , 'variant12' , 'variant11'],
         mouth: ['variant30' , 'variant29' , 'variant28' , 'variant27' , 'variant26' , 'variant25' , 'variant24' , 'variant23' , 'variant22' , 'variant21' , 'variant20' , 'variant19' , 'variant18' , 'variant17' , 'variant16' , 'variant15' , 'variant14' , 'variant13' , 'variant12' , 'variant11' , 'variant10' , 'variant09' , 'variant08' , 'variant07' , 'variant06' , 'variant05' , 'variant04' , 'variant03' , 'variant02' , 'variant01'],
         hair: ['short16' , 'short15' , 'short14' , 'short13' , 'short12' , 'short11' , 'short10' , 'short09' , 'short08' , 'short07' , 'short06' , 'short05' , 'short04' , 'short03' , 'long20' , 'short02' , 'short01' , 'long19' , 'long18' , 'long17' , 'long16' , 'long15' , 'long14' , 'long13' , 'long12' , 'long11' , 'long10' , 'long09' , 'long08' , 'long07' , 'long06' , 'long05' , 'long04' , 'long03' , 'long02' , 'long01' , 'short19' , 'long26' , 'long25' , 'short18' , 'long24' , 'long23' , 'long22' , 'short17' , 'long21'],
-        earrings: ['variant06' , 'variant01' , 'variant02' , 'variant03' , 'variant04' , 'variant05'],
         skinColor: ["9e5622","763900","ecad80"],
         hairColor: ["0e0e0e","3eac2c","6a4e35","85c2c6","796a45","562306","592454","ab2a18","ac6511","afafaf","b9a05f","cb6820","dba3be","e5d7a3"]
     });
