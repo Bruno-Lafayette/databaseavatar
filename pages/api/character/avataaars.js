4b0d1a28674bdc261a00185dea3551281190a427
Bruno.Lafayette <brunolafayette@rocketmail.com>
2023-02-13 15:21:18 -0300

    alterado avatar

--- pages/api/character/avataaars.js
+++ pages/api/character/avataaars.js
@@ -5,13 +5,8 @@ export default function avataaars(req, res){
             eyes: ['closed' , 'cry' , 'default' , 'eyeRoll' , 'happy' , 'hearts' , 'side' , 'squint' , 'surprised' , 'winkWacky' , 'wink' , 'xDizzy'],
             eyebrows: ['angryNatural' , 'defaultNatural' , 'flatNatural' , 'frownNatural' , 'raisedExcitedNatural' , 'sadConcernedNatural' , 'unibrowNatural' , 'upDownNatural' , 'angry' , 'default' , 'raisedExcited' , 'sadConcerned' , 'upDown'],
             top: ['eyepatch' , 'hat' , 'hijab' , 'turban' , 'winterHat1' , 'winterHat02' , 'winterHat03' , 'winterHat04' , 'bob' , 'bun' , 'curly' , 'curvy' , 'dreads' , 'frida' , 'fro' , 'froBand' , 'longButNotTooLong' , 'miaWallace' , 'shavedSides' , 'straight02' , 'straight01' , 'straightAndStrand' , 'dreads01' , 'dreads02' , 'frizzle' , 'shaggy' , 'shaggyMullet' , 'shortCurly' , 'shortFlat' , 'shortRound' , 'shortWaved' , 'sides' , 'theCaesar' , 'theCaesarAndSidePart' , 'bigHair'],
-            facialHair: ['beardLight' , 'beardMagestic' , 'beardMedium' , 'moustacheFancy' , 'moustacheMagnum'],
-            accessories: ['kurt' , 'prescription01' , 'prescription02' , 'round' , 'sunglasses' , 'wayfarers'],
-            clothingGraphic: ['bat' , 'bear' , 'cumbia' , 'deer' , 'diamond' , 'hola' , 'pizza' , 'resist' , 'skull' , 'skullOutline'],
-            hatColor: String,
-            hairColor: String,
-            skinColor: String,
-            facialHairColor: String,
-            backgroundColor: String,
+            hatColor: ["0e0e0e","3eac2c","6a4e35","85c2c6","796a45","562306","592454","ab2a18","ac6511","afafaf","b9a05f","cb6820","dba3be","e5d7a3"],
+            hairColor: ["0e0e0e","3eac2c","6a4e35","85c2c6","796a45","562306","592454","ab2a18","ac6511","afafaf","b9a05f","cb6820","dba3be","e5d7a3"],
+            skinColor: ["9e5622","763900","ecad80"]
     })
 }
--- pages/api/character/lorelei.js
+++ pages/api/character/lorelei.js
@@ -6,8 +6,6 @@ export default function lorelei(req,res){
     eyes: ['variant24' , 'variant23' , 'variant22' , 'variant21' , 'variant20' , 'variant19' , 'variant18' , 'variant17' , 'variant16' , 'variant15' , 'variant14' , 'variant13' , 'variant12' , 'variant11' , 'variant10' , 'variant09' , 'variant08' , 'variant07' , 'variant06' , 'variant05' , 'variant04' , 'variant03' , 'variant02' , 'variant01'],
     eyebrows: ['variant13' , 'variant12' , 'variant11' , 'variant10' , 'variant09' , 'variant08' , 'variant07' , 'variant06' , 'variant05' , 'variant04' , 'variant03' , 'variant02' , 'variant01'],
     nose: ['variant01' , 'variant02' , 'variant03' , 'variant04' , 'variant05' , 'variant06'],
-    beard: ['variant01' , 'variant02'],
     mouth: ['happy01' , 'happy02' , 'happy03' , 'happy04' , 'happy05' , 'happy06' , 'happy07' , 'happy08' , 'happy18' , 'happy09' , 'happy10' , 'happy11' , 'happy12' , 'happy13' , 'happy14' , 'happy17' , 'happy15' , 'happy16' , 'sad01' , 'sad02' , 'sad03' , 'sad04' , 'sad05' , 'sad06' , 'sad07' , 'sad08' , 'sad09'],
-    glasses: ['variant01' , 'variant02' , 'variant03' , 'variant04' , 'variant05'],
 })
 }
