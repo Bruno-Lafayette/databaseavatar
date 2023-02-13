9feb2de2581ff26f1365810e61ba69535586126e
Bruno.Lafayette <brunolafayette@rocketmail.com>
2023-02-13 15:49:45 -0300

    alterado avatar

---
+++ pages/api/character/identicon.js
@@ -0,0 +1,10 @@
+export default function identicon (res,rep){
+    res.json({
+        row1: ['xooox' , 'xxoxx' , 'xoxox' , 'oxxxo' , 'xxxxx' , 'oxoxo' , 'ooxoo'],
+        row2: ['xooox' , 'xxoxx' , 'xoxox' , 'oxxxo' , 'xxxxx' , 'oxoxo' , 'ooxoo'],
+        row3: ['xooox' , 'xxoxx' , 'xoxox' , 'oxxxo' , 'xxxxx' , 'oxoxo' , 'ooxoo'],
+        row4: ['xooox' , 'xxoxx' , 'xoxox' , 'oxxxo' , 'xxxxx' , 'oxoxo' , 'ooxoo'],
+        row5: ['xooox' , 'xxoxx' , 'xoxox' , 'oxxxo' , 'xxxxx' , 'oxoxo' , 'ooxoo'],
+        rowColor: ["00acc1","fdd835","d81b60", "8e24aa","00897b"],
+    })
+}
