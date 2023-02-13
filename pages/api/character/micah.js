13c3e094680ecfd792bde4829e9a9012d02786f9
Bruno.Lafayette <brunolafayette@rocketmail.com>
2023-02-13 16:00:25 -0300

    alterado avatar

---
+++ pages/api/character/micah.js
@@ -0,0 +1,24 @@
+export default function micah(res,rep) {
+
+    res.json({
+
+        mouth: ['surprised' ,'laughing' ,'nervous' ,'smile' ,'sad' ,'pucker' ,'frown' ,'smirk'],
+        eyebrows: ['up' ,'down' ,'eyelashesUp' ,'eyelashesDown'],
+        hair: ['fonze' ,'mrT' ,'dougFunny' ,'mrClean' ,'dannyPhantom' ,'full' ,'turban' ,'pixie'],
+        eyes: ['eyes' ,'round' ,'eyesShadow' ,'smiling'],
+        nose: ['curve' ,'pointed' ,'tound'],
+        ears: ['attached' ,'detached'],
+        shirt: ['open' ,'crew' ,'collared'],
+        earrings: ['hoop' ,'stud'],
+        baseColor: ["0e0e0e","3eac2c","6a4e35","85c2c6","796a45","562306","592454","ab2a18","ac6511","afafaf","b9a05f","cb6820","dba3be","e5d7a3"],
+        eyeShadowColor: ["0e0e0e","3eac2c","6a4e35","85c2c6","796a45","562306","592454","ab2a18","ac6511","afafaf","b9a05f","cb6820","dba3be","e5d7a3"],
+        eyebrowsColor: ["0e0e0e","3eac2c","6a4e35","85c2c6","796a45","562306","592454","ab2a18","ac6511","afafaf","b9a05f","cb6820","dba3be","e5d7a3"],
+        hairColor: ["0e0e0e","3eac2c","6a4e35","85c2c6","796a45","562306","592454","ab2a18","ac6511","afafaf","b9a05f","cb6820","dba3be","e5d7a3"],
+        mouthColor: ["0e0e0e","3eac2c","6a4e35","85c2c6","796a45","562306","592454","ab2a18","ac6511","afafaf","b9a05f","cb6820","dba3be","e5d7a3"],
+        shirtColor: ["0e0e0e","3eac2c","6a4e35","85c2c6","796a45","562306","592454","ab2a18","ac6511","afafaf","b9a05f","cb6820","dba3be","e5d7a3"],
+        eyesColor: ["0e0e0e","3eac2c","6a4e35","85c2c6","796a45","562306","592454","ab2a18","ac6511","afafaf","b9a05f","cb6820","dba3be","e5d7a3"]
+
+    })
+
+
+}
